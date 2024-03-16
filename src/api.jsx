33628fdbbfe4a23087f7b884d22070ab05ac82a5
mazen41 <161659553+mazen41@users.noreply.github.com>
2024-03-16 16:20:44 +0200

    Updating

--- src/api.jsx
+++ src/api.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: "https://coffee-online-store.infinityfreeapp.com/",
     withCredentials: true,
-    headers: {
-        'Content-Type': 'multipart/form-data',
-    },
+    // headers: {
+    //     'Content-Type': 'multipart/form-data',
+    // },
 
 });
 
